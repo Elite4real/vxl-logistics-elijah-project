8c898bc250bd0d47af5652b1575d12473e13013d
Elite4real <101645675+Elite4real@users.noreply.github.com>
2022-05-06 01:12:00 +0100

    Add files via upload

---
+++ page_one e.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+
+class Googlepixel21Widget extends StatelessWidget {
+          @override
+          Widget build(BuildContext context) {
+          // Figma Flutter Generator Googlepixel21Widget - FRAME
+            return Container(
+      width: 411,
+      height: 823,
+      decoration: BoxDecoration(
+          color : Color.fromRGBO(22, 127, 96, 1),
+  ),
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+        top: 294,
+        left: 106.9715576171875,
+        child: Container(
+      width: 196.24264526367188,
+      height: 119.30467224121094,
+      
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+        top: 0.006477401591837406,
+        left: 76.9444580078125,
+        child: SvgPicture.asset(
+        'assets/images/vector.svg',
+        semanticsLabel: 'vector'
+      );
+      ),Positioned(
+        top: 1.940780520439148,
+        left: 59.28947067260742,
+        child: SvgPicture.asset(
+        'assets/images/vector.svg',
+        semanticsLabel: 'vector'
+      );
+      ),Positioned(
+        top: 0.006477401591837406,
+        left: 38.472145080566406,
+        child: SvgPicture.asset(
+        'assets/images/vector.svg',
+        semanticsLabel: 'vector'
+      );
+      ),Positioned(
+        top: 0,
+        left: 19.819303512573242,
+        child: SvgPicture.asset(
+        'assets/images/vector.svg',
+        semanticsLabel: 'vector'
+      );
+      ),Positioned(
+        top: 0.006477401591837406,
+        left: 0,
+        child: SvgPicture.asset(
+        'assets/images/vector.svg',
+        semanticsLabel: 'vector'
+      );
+      ),
+        ]
+      )
+    )
+      ),Positioned(
+        top: 442,
+        left: 79,
+        child: Text('VFX LOGISTICS', textAlign: TextAlign.left, style: TextStyle(
+        color: Color.fromRGBO(255, 255, 255, 1),
+        fontFamily: 'Open Sans',
+        fontSize: 36,
+        letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+        fontWeight: FontWeight.normal,
+        height: 1
+      ),)
+      ),Positioned(
+        top: 491,
+        left: 96,
+        child: Text('....we move anything, seriously.', textAlign: TextAlign.left, style: TextStyle(
+        color: Color.fromRGBO(255, 255, 255, 1),
+        fontFamily: 'Open Sans',
+        fontSize: 14,
+        letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+        fontWeight: FontWeight.normal,
+        height: 1
+      ),)
+      ),
+        ]
+      )
+    );
+          }
+        }
+        
---
+++ page_three e.dart
@@ -0,0 +1,158 @@
+import 'package:flutter/material.dart';
+
+
+class Googlepixel24Widget extends StatelessWidget {
+          @override
+          Widget build(BuildContext context) {
+          // Figma Flutter Generator Googlepixel24Widget - FRAME
+            return Container(
+      width: 411,
+      height: 823,
+      decoration: BoxDecoration(
+          color : Color.fromRGBO(255, 255, 255, 1),
+  ),
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+        top: 677,
+        left: 16,
+        child: Container(
+        width: 379,
+        height: 44,
+        decoration: BoxDecoration(
+          borderRadius : BorderRadius.only(
+            topLeft: Radius.circular(6),
+            topRight: Radius.circular(6),
+            bottomLeft: Radius.circular(6),
+            bottomRight: Radius.circular(6),
+          ),
+      color : Color.fromRGBO(26, 147, 111, 1),
+  )
+      )
+      ),Positioned(
+        top: 729,
+        left: 16,
+        child: Container(
+        width: 379,
+        height: 44,
+        decoration: BoxDecoration(
+          borderRadius : BorderRadius.only(
+            topLeft: Radius.circular(6),
+            topRight: Radius.circular(6),
+            bottomLeft: Radius.circular(6),
+            bottomRight: Radius.circular(6),
+          ),
+      border : Border.all(
+          color: Color.fromRGBO(196, 196, 196, 1),
+          width: 0.5,
+        ),
+  )
+      )
+      ),Positioned(
+        top: 690,
+        left: 183,
+        child: Text('LOGIN', textAlign: TextAlign.left, style: TextStyle(
+        color: Color.fromRGBO(255, 255, 255, 1),
+        fontFamily: 'Open Sans',
+        fontSize: 14,
+        letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+        fontWeight: FontWeight.normal,
+        height: 1
+      ),)
+      ),Positioned(
+        top: 742,
+        left: 177,
+        child: Text('SIGN UP', textAlign: TextAlign.left, style: TextStyle(
+        color: Color.fromRGBO(17, 75, 95, 1),
+        fontFamily: 'Open Sans',
+        fontSize: 14,
+        letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+        fontWeight: FontWeight.normal,
+        height: 1
+      ),)
+      ),Positioned(
+        top: 63,
+        left: -44,
+        child: Container(
+        width: 500,
+        height: 500,
+        decoration: BoxDecoration(
+          image : DecorationImage(
+          image: AssetImage('assets/images/Onboardingdesignartboard60copy23x.png'),
+          fit: BoxFit.fitWidth
+      ),
+  )
+      )
+      ),Positioned(
+        top: 79,
+        left: 144,
+        child: Text('Delivery', textAlign: TextAlign.left, style: TextStyle(
+        color: Color.fromRGBO(25, 25, 25, 1),
+        fontFamily: 'Open Sans',
+        fontSize: 30,
+        letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+        fontWeight: FontWeight.normal,
+        height: 1
+      ),)
+      ),Positioned(
+        top: 0,
+        left: 0,
+        child: Container(
+        width: 411,
+        height: 20,
+        decoration: BoxDecoration(
+          color : Color.fromRGBO(0, 0, 0, 0.07000000029802322),
+  )
+      )
+      ),Positioned(
+        top: 571,
+        left: 183,
+        child: Container(
+        width: 10,
+        height: 10,
+        decoration: BoxDecoration(
+          color : Color.fromRGBO(196, 196, 196, 1),
+      borderRadius : BorderRadius.all(Radius.elliptical(10, 10)),
+  )
+      )
+      ),Positioned(
+        top: 571,
+        left: 201,
+        child: Container(
+        width: 10,
+        height: 10,
+        decoration: BoxDecoration(
+          color : Color.fromRGBO(196, 196, 196, 1),
+      borderRadius : BorderRadius.all(Radius.elliptical(10, 10)),
+  )
+      )
+      ),Positioned(
+        top: 571,
+        left: 219,
+        child: Container(
+        width: 10,
+        height: 10,
+        decoration: BoxDecoration(
+          color : Color.fromRGBO(26, 147, 111, 1),
+      borderRadius : BorderRadius.all(Radius.elliptical(10, 10)),
+  )
+      )
+      ),Positioned(
+        top: 495,
+        left: 58,
+        child: Text('vfx logistics brings you closer to 
+your customers and blahh blahh here.', textAlign: TextAlign.center, style: TextStyle(
+        color: Color.fromRGBO(102, 102, 102, 1),
+        fontFamily: 'Open Sans',
+        fontSize: 16,
+        letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+        fontWeight: FontWeight.normal,
+        height: 1
+      ),)
+      ),
+        ]
+      )
+    );
+          }
+        }
+        
